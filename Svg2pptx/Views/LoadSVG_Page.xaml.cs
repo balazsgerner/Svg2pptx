9039bcb95be9f963fa389912dfc2670e3d9e385d
Balázs Gerner <balazsgerner@gmail.com>
2018-10-27 14:05:11 +0200

    Fixing layout, memory leaks

--- Svg2pptx/Views/LoadSVG_Page.xaml.cs
+++ Svg2pptx/Views/LoadSVG_Page.xaml.cs
@@ -29,9 +29,12 @@ namespace Svg2pptx.Views
     public sealed partial class LoadSVGPage : Page
     {
 
+        private SvgImageSource ImageSource { get; set; }
+
         public LoadSVGPage()
         {
             this.InitializeComponent();
+            ImageSource = new SvgImageSource();
         }
 
 
@@ -71,48 +74,35 @@ namespace Svg2pptx.Views
             }
         }
 
-        private static async Task<ImageSource> LoadImageSource(StorageFile file)
+        private async Task<ImageSource> LoadImageSource(StorageFile file)
         {
-            SvgImageSource src = new SvgImageSource();
-            await src.SetSourceAsync(await file.OpenReadAsync());
-            return src;
+            await ImageSource.SetSourceAsync(await file.OpenReadAsync());
+            return ImageSource;
         }
 
         public string PrintXML(string xml)
         {
             string result = string.Empty;
             XmlDocument document = new XmlDocument();
-
             using (var mStream = new MemoryStream())
             using (var writer = new XmlTextWriter(mStream, System.Text.Encoding.Unicode))
+            using (var sReader = new StreamReader(mStream))
             {
                 try
                 {
-                    // Load the XmlDocument with the XML.
                     document.LoadXml(xml);
-
                     writer.Formatting = Formatting.Indented;
 
-                    // Write the XML into a formatting XmlTextWriter
                     document.WriteContentTo(writer);
                     writer.Flush();
                     mStream.Flush();
 
-                    // Have to rewind the MemoryStream in order to read
-                    // its contents.
                     mStream.Position = 0;
-
-                    // Read MemoryStream contents into a StreamReader.
-                    StreamReader sReader = new StreamReader(mStream);
-
-                    // Extract the text from the StreamReader.
-                    string formattedXml = sReader.ReadToEnd();
-
-                    result = formattedXml;
+                    result = sReader.ReadToEnd();
                 }
                 catch (XmlException)
                 {
-                    // Handle the exception
+                    // Error occured while printing XML content
                 }
 
             }
